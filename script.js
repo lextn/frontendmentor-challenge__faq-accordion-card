90cd3151324d36425ef6fb93538a8bc00b9b7c0f
lextn <lextnmail@gmail.com>
2021-10-14 21:13:01 +0300

    uploaded

---
+++ script.js
@@ -0,0 +1,19 @@
+const toggleAnswer = (e) => {
+  const question = e.childNodes[1];
+  hideAllAnswers(question);
+  e.childNodes[3].classList.toggle("rotate-icon");
+  e.classList.toggle("bold");
+  e.nextElementSibling.classList.toggle("hide");
+};
+
+const hideAllAnswers = (question) => {
+  const questionsEl = [...document.querySelectorAll(".question")];
+  questionsEl.forEach((questionEl) => {
+    console.log();
+    if (questionEl.childNodes[1] != question) {
+      questionEl.classList.remove("bold");
+      questionEl.nextElementSibling.classList.add("hide");
+      questionEl.childNodes[3].classList.remove("rotate-icon");
+    }
+  });
+};
